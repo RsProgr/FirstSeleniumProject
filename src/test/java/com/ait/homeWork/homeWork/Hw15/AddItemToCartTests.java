68ea22ce2b86093083845b482c640266ffbcd75d
RsProgr <roma19@list.ru>
2025-03-02 17:42:13 +0200

    upd HW16

--- src/test/java/com/ait/homeWork/homeWork/Hw15/AddItemToCartTests.java
+++ src/test/java/com/ait/homeWork/homeWork/Hw15/AddItemToCartTests.java
@@ -1,15 +1,8 @@
 package com.ait.homeWork.homeWork.Hw15;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 public class AddItemToCartTests extends TestBase {
 
@@ -20,22 +13,17 @@ public class AddItemToCartTests extends TestBase {
 
     @Test
     public void addItemToCartTest() {
-        click(By.cssSelector("img[src='https://demowebshop.tricentis.com/content/images/thumbs/0000224_141-inch-laptop_125.png']"));
-        click(By.cssSelector("#add-to-cart-button-31"));
-        Assert.assertEquals(isElementPresent(By.cssSelector(".cart-qty")), true);
+        clickProductPage();
+        clickAddItemToCart();
+        verifyItemAddedToCart();
     }
 
     @Test
     public void removeProductFromCart() {
-        click(By.cssSelector(".cart-label"));
-        click(By.cssSelector("[type=checkbox]"));
-        click(By.cssSelector("[name='updatecart']"));
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement emptyCartMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//*[contains(text(), 'Your Shopping Cart is empty')]")));
-        String actualText = emptyCartMessage.getText();
-        Assert.assertEquals("Your Shopping Cart is empty!", actualText);
+        clickShoppingCartLink();
+        clickChekbox();
+        clickUpdateShoppingCartButton();
+        verifyCartIsEmpty();
     }
 
     @AfterMethod(enabled = false)
--- src/test/java/com/ait/homeWork/homeWork/Hw15/TestBase.java
+++ src/test/java/com/ait/homeWork/homeWork/Hw15/TestBase.java
@@ -2,7 +2,10 @@ package com.ait.homeWork.homeWork.Hw15;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -91,4 +94,39 @@ public class TestBase {
     public void clickOnLoginLink() {
         click(By.cssSelector("[href='/login']"));
     }
+
+    protected void verifyItemAddedToCart() {
+        Assert.assertEquals(isElementPresent(By.cssSelector(".cart-qty")), true);
+    }
+
+    protected void clickAddItemToCart() {
+        click(By.cssSelector("#add-to-cart-button-31"));
+    }
+
+    protected void clickProductPage() {
+        click(By.cssSelector("img[src='https://demowebshop.tricentis.com/content/images/thumbs/0000224_141-inch-laptop_125.png']"));
+    }
+
+    protected void verifyCartIsEmpty() {
+        Assert.assertEquals(getEmptyCartMessage(), "Your Shopping Cart is empty!", "Cart should be empty after removing an item");
+    }
+
+    private String getEmptyCartMessage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement emptyCartMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//*[contains(text(), 'Your Shopping Cart is empty')]")));
+        return emptyCartMessage.getText();
+    }
+
+    protected void clickUpdateShoppingCartButton() {
+    click(By.cssSelector("[name='updatecart']"));
+}
+
+    protected void clickChekbox() {
+    click(By.cssSelector("[type=checkbox]"));
+}
+
+    protected void clickShoppingCartLink() {
+    click(By.cssSelector(".cart-label"));
+}
 }
